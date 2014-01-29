877a605ccb0b78c40e251424caafa63d8e57de1e
petejh <petejh@truly.fake>
2014-01-29 17:05:58 +0000

    Add /:username route to user profile

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Twetter::Application.routes.draw do
   authenticated :user do
     resources :follows, :except => [:new, :edit, :show, :update]
     resources :twets, :except => [:new, :edit, :show, :update]
+    get ':username', to: 'users#show', :as => :user
     root :to => 'follows#index', :as => :user_root
   end
 
