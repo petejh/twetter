7418ba1efd64554350e39a14b9f8cb3b18b4a1c8
petejh <petejh@truly.fake>
2014-01-30 00:04:18 +0000

    Check that the username is valid before showing the user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,20 @@
 class UsersController < ApplicationController
   # All actions in this controller require the presence of an authenticated user.
   before_filter :authenticate_user!
+
+  before_filter :username_exists?, only: :show
   
   def show
   end
   
   private
+  def username_exists?
+    unless User.exists?({ :username => params[:username] })
+      flash[:error] = "Invalid username"
+      redirect_to :root
+    end
+  end
+  
   def user_params
     params.require(:user).permit(:username)
   end
