cdd85b1470549810b352baa0130a266f3d7ad31b
petejh <petejh@truly.fake>
2014-02-07 02:56:04 +0000

    Add the user's gravatar to the left nav panel

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,3 +1,5 @@
+require 'digest/md5'
+
 module UsersHelper
   def link_mentions(content)
     content.gsub(/@\S*/) do |mention|
@@ -8,5 +10,10 @@ module UsersHelper
         html_escape_once(mention)
       end
     end
+  end
+  
+  def gravatar_url(user)
+    hash = Digest::MD5.hexdigest(user.email.downcase)
+    "http://www.gravatar.com/avatar/#{hash}"
   end
 end
