3ae33bdba1c7ed29093c1403bfb8a4e241611dec
petejh <petejh@truly.fake>
2014-02-07 00:32:57 +0000

    Refactor user profile and twets index to use a shared view for each twet
    Now all @mentions in both views will render links to that user's profile.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,12 @@
 module UsersHelper
   def link_mentions(content)
     content.gsub(/@\S*/) do |mention|
-      username = mention.gsub(/@/,'')
-      link_to mention, user_path(username)
+      username = mention.delete "@"
+      if User.exists?(username: username)
+        link_to mention, user_path(username)
+      else
+        html_escape_once(mention)
+      end
     end
   end
 end
